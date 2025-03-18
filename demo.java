c28eb73b2019ed6a74ee23930003272fae13f419
raushan <raushank80828@gmail.com>
2025-03-18 18:59:27 +0530

    change in demo file

--- demo.java
+++ demo.java
@@ -1,7 +1,12 @@
-class demo{
+class demo
+{
     public static void main(String[] args)
     {
         System.out.println("hello");
 
+    }
+    public void print()
+    {
+        System.out.println("print");
     }
 }
